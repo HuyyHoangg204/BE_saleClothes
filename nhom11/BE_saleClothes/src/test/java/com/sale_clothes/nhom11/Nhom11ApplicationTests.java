16c717667d517333ae65239faa5e264c95adff2b
huyhoang <tranhuyyhoang2004@gmail.com>
2024-09-18 18:20:20 +0700

    fix: rebuild base project

---
+++ nhom11/BE_saleClothes/src/test/java/com/sale_clothes/nhom11/Nhom11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sale_clothes.nhom11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
