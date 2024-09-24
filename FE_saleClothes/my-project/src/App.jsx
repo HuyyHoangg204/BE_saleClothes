79e3406cd76cdfcc41ab616af1cecc3dd35b0bca
huyhoang <tranhuyyhoang2004@gmail.com>
2024-09-24 12:13:36 +0700

    feat: new ui/ux login

--- FE_saleClothes/my-project/src/App.jsx
+++ FE_saleClothes/my-project/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
+
 import "./App.css";
-import Navigation from "./customers/components/Navigation/Navigation";
-import HomePage from "./customers/components/pages/HomePage/HomePage";
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <div>
-      <Navigation />
-      <div>
-        <HomePage />
-      </div>
+      
     </div>
   );
 }
