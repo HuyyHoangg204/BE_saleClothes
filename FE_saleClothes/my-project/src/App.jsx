ee0be9094731910200041f6ed7894fccea4f1626
huyhoang <tranhuyyhoang2004@gmail.com>
2024-09-25 19:44:53 +0700

    feat: finish feature register

--- FE_saleClothes/my-project/src/App.jsx
+++ FE_saleClothes/my-project/src/App.jsx
@@ -1,7 +1,7 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-
-
+import Login from "./customers/components/Login/Login.jsx";
+import Register from"./customers/components/Register/Register.jsx";
 
 function App() {
 
@@ -9,6 +9,14 @@ function App() {
   return (
     <div>
       
+      <BrowserRouter>
+      <Routes>
+        {/* // http://localhost:5173 */}
+        <Route path="/login" element = {<Login/>}></Route> 
+        <Route path="/register" element = {<Register/>}></Route>
+      </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
