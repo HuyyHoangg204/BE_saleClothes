8384a530071e4e12f0ced8d8b442b01830bd9477
Huy Hoàng <tranhuyyhoang2004@gmail.com>
2024-10-22 15:08:10 +0700

    v1.0.0 fix

--- nhom11/BE_saleClothes/src/main/java/com/sale_clothes/nhom11/configuration/SecurityConfig.java
+++ nhom11/BE_saleClothes/src/main/java/com/sale_clothes/nhom11/configuration/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://51.79.167.161");
+        corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
